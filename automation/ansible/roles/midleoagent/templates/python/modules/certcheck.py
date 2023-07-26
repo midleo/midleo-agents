a32204b9a4b88899b0969704ff2946b939c10085
Vasilev <vasil@vasilev.link>
2023-07-26 14:43:46 +0200

    fixed runmqakm command

--- automation/ansible/roles/midleoagent/templates/python/modules/certcheck.py
+++ automation/ansible/roles/midleoagent/templates/python/modules/certcheck.py
@@ -18,8 +18,8 @@ def Run(certs):
          certcn = subprocess.check_output("keytool -list -v -keystore "+cfile+" -storepass "+cpass+" -alias "+clabel+" | grep 'Owner' | grep -o -P '(?<=CN=).*(?=, OU)'", shell=True).strip().decode("utf-8").replace('  ','')
          certvalid = subprocess.check_output("keytool -list -v -keystore "+cfile+" -storepass "+cpass+" -alias "+clabel+" | grep 'until' | sed -n -e 's/^.*until: //p'", shell=True).strip().decode("utf-8")
        if(command=="runmqakm"):
-         certcn = subprocess.check_output("runmqakm -cert -details -db "+cfile+" -label "+clabel+" -stashed | grep 'Owner' | grep -o -P '(?<=CN=).*(?=, OU)'", shell=True).strip().decode("utf-8")
-         certvalid = subprocess.check_output("runmqakm -cert -details -db "+cfile+" -label "+clabel+" -stashed | grep 'Not After' | sed -n -e 's/^.*Not After: //p'", shell=True).strip().decode("utf-8")
+         certcn = subprocess.check_output("runmqakm -cert -details -db "+cfile+" -label "+clabel+" -stashed | grep 'CN' | grep 'Subject' | sed -n -e 's/^.*CN=//p'", shell=True).strip().decode("utf-8")
+         certvalid = subprocess.check_output("runmqakm -cert -details -db "+cfile+" -label "+clabel+" -stashed | grep 'Valid' | sed -n -e 's/^.*To: //p'", shell=True).strip().decode("utf-8")
        if bool(certcn):
          certdetails={"CN":certcn,"VALID":certvalid,"FILE":cfile}
          data.append(json.loads(json.dumps(certdetails)))
