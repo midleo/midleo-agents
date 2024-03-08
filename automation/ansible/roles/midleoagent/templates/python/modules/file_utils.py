b9ff3a4def9da94d0b80fb984ab974021e927ec6
Vasilev <vasil@vasilev.link>
2024-03-08 21:29:03 +0100

    replaced with csv read

--- automation/ansible/roles/midleoagent/templates/python/modules/file_utils.py
+++ automation/ansible/roles/midleoagent/templates/python/modules/file_utils.py
@@ -1,4 +1,4 @@
-import json, os
+import json, os, csv
 from modules import classes
 
 def csv_json(file,array,check=False,cleanit=False):
@@ -8,32 +8,32 @@ def csv_json(file,array,check=False,cleanit=False):
             try:
                with open(file) as f:
                   json_arr = []
-                  for line in f:
-                     linearr=line.strip().split(',')
+                  reader_obj = csv.reader((line.replace('\0','') for line in f), delimiter = ',')
+                  for linearr in reader_obj:
                      if(len(linearr)<2): 
                         pass
                      else:
                        in_arr={}
                        if(check and linearr[0]==check):
-                         in_arr["type"]=array["type"]
-                         in_arr["line"]=str("sumarized" if str(linearr[0])=='summary' else linearr[0])
-                         for (key, value) in array.items():
+                          in_arr["type"]=array["type"]
+                          in_arr["line"]=str("sumarized" if linearr[0]=='summary' else linearr[0])
+                          for (key, value) in array.items():
                             try:
                                in_arr[key]=linearr[int(value)]
                             except:
                                pass
-                         json_arr.append(in_arr)
+                          json_arr.append(in_arr)
                        else:
-                         pass
-                     if(not check and linearr[0]!=array["noteq"]):
-                        in_arr["type"]=array["type"]
-                        in_arr["line"]=str("sumarized" if str(linearr[0])=='summary' else linearr[0])
-                        for (key, value) in array.items():
+                          pass
+                       if(not check and linearr[0]!=array["noteq"]):
+                          in_arr["type"]=array["type"]
+                          in_arr["line"]=str("sumarized" if linearr[0]=='summary' else linearr[0])
+                          for (key, value) in array.items():
                             try:
                                in_arr[key]=linearr[int(value)]
                             except:
                                pass
-                        json_arr.append(in_arr)
+                          json_arr.append(in_arr)
             except OSError as err:
                classes.Err("Error opening the file:"+str(err))
         if(cleanit):
