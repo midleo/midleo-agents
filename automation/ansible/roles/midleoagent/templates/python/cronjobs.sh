8259fc556cba87444f606878c488e36df17bf3c8
Vasilev <vasil@vasilev.link>
2024-06-06 15:27:26 +0200

    migrated to mwagent

--- automation/ansible/roles/midleoagent/templates/python/cronjobs.sh
+++ automation/ansible/roles/midleoagent/templates/python/cronjobs.sh
@@ -5,6 +5,7 @@
 #https://vasilev.link
 
 cd $(dirname $0)
+USR=`whoami`
 
 HOMEDIR=$(pwd)"/config"
 DSPMQVER=/opt/mqm/bin/dspmqver
@@ -25,8 +26,6 @@ YM=$(date '+%Y-%m')
 WD=$(date '+%d')
 HOUR=$(date '+%H%M')
 
-cd $(dirname $0)
-
 if [[ ! -e $HOMEDIR ]]; then
     mkdir $HOMEDIR
 fi
@@ -35,44 +34,7 @@ load_config ()
 {
    if [ -e "$HOMEDIR/midleoclient.conf" ]; then
       . $HOMEDIR/midleoclient.conf
-   else
-      create_config
    fi
-}
-create_config (){
-    echo "Midleo DNS server name:"
-    read midleodns
-    if [ -z "${midleodns}" ]; then
-       rm $HOMEDIR/"midleoclient.conf"
-    else
-       echo "midleodns=$midleodns" > $HOMEDIR/"midleoclient.conf"
-    fi
-    echo "IBM MQ library path(example - /opt/mqm/java/lib64):"
-    read ibmmqlibpath
-    if [ -z "${ibmmqlibpath}" ]; then
-       rm $HOMEDIR/"midleoclient.conf"
-    else
-       echo ibmmqlibpath=$ibmmqlibpath >> $HOMEDIR/"midleoclient.conf"
-    fi
-    if test -f "$DSPMQVER"; then
-      mainver=`$DSPMQVER | grep Version | awk '{print $2}' | cut -d '.' -f 1`
-      echo mainver=$mainver >> $HOMEDIR/"midleoclient.conf"
-    fi
-    if [ -n "$mainver" ] && [ $mainver -gt 8 ]; then
-      echo mqwebhost=`hostname -f` >> $HOMEDIR/"midleoclient.conf"
-      echo "IBM MQWeb user:"
-      read ibmmqwebusr
-      echo ibmmqwebusr=$ibmmqwebusr >> $HOMEDIR/"midleoclient.conf"
-      echo "IBM MQWeb password:"
-      read -s ibmmqwebusrpwd
-      echo ibmmqwebusrpwd=`echo $ibmmqwebusrpwd | base64`  >> $HOMEDIR/"midleoclient.conf"
-      echo "IBM MQWeb SSL enabled(y/n):"
-      read ibmmqwebssl
-      echo ibmmqwebssl=$ibmmqwebssl >> $HOMEDIR/"midleoclient.conf"
-      echo "IBM MQWeb port:"
-      read mqwebport
-      echo mqwebport=$mqwebport >> $HOMEDIR/"midleoclient.conf"
-    fi
 }
 
 runmqjob (){
@@ -226,67 +188,6 @@ EOF
 fi
 }
 
-enabletrackqm (){
-/usr/bin/python3 << EOF
-import base64,platform,json,re,uuid,time,subprocess,socket,sys,os
-from datetime import datetime
-from modules import makerequest,classes,configs
-
-if platform.system()=="Linux":
-   from modules import lin_utils,lin_packages
-elif platform.system()=="Windows":
-   from modules import win_utils
-else:
-   exit()
-
-QMGR="$1"
-
-def createTrackJson():
-   try:
-      track_data = configs.gettrackData()
-   except Exception as err:
-      track_data = {}
-
-   track_data[QMGR] = {
-    "type": "enabled"
-   }
-
-   with open(os.getcwd()+"/config/conftrack.json", 'w+') as track_file:
-      json.dump(track_data, track_file)
-   print(QMGR+" have been added")
-    
-if __name__ == "__main__":
-   createTrackJson()
-
-EOF
-}
-
-disabletrackqm (){
-/usr/bin/python3 << EOF
-import base64,platform,json,re,uuid,time,subprocess,socket,sys,os
-from datetime import datetime
-from modules import makerequest,classes,configs
-
-if platform.system()=="Linux":
-   from modules import lin_utils,lin_packages
-elif platform.system()=="Windows":
-   from modules import win_utils
-else:
-   exit()
-
-QMGR="$1"
-
-try:
-   track_data = configs.gettrackData()
-except Exception as err:
-   print("No such configuration file - config/conftrack.json")
-track_data.pop(QMGR, None)
-with open(os.getcwd()+"/config/conftrack.json", 'w+') as track_file:
-   json.dump(track_data, track_file)
-print(QMGR+" configuration deleted")
-EOF
-}
-
 runmqmon(){
 /usr/bin/python3 << EOF
 import base64,platform,json,re,uuid,time,subprocess,socket,sys,os
@@ -353,142 +254,21 @@ if __name__ == "__main__":
 EOF
 }
 
-addmon(){
-/usr/bin/python3 << EOF
-import base64,platform,json,re,uuid,time,subprocess,socket,sys,os
-from datetime import datetime
-from modules import makerequest,classes,configs
-
-if platform.system()=="Linux":
-   from modules import lin_utils,lin_packages
-elif platform.system()=="Windows":
-   from modules import win_utils
-else:
-   exit()
-
-QMGR="$1"
-QUEUE="$2"
-TMON="$3"
-THRES="$4"
-
-def createMonJson():
-   try:
-      mon_data = configs.getmonData()
-   except Exception as err:
-      mon_data = {}
-   try:
-     mon_data[QMGR][QUEUE] = {
-      "type": TMON,
-      "thres": THRES
-     }
-   except Exception as err: 
-     mon_data[QMGR] = {}
-     mon_data[QMGR][QUEUE] = {
-      "type": TMON,
-      "thres": THRES
-     }
-
-   with open(os.getcwd()+"/config/confmon.json", 'w+') as mon_file:
-      json.dump(mon_data, mon_file)
-   print(QUEUE+" on "+QMGR+" have been added")
-    
-if __name__ == "__main__":
-   createMonJson()
-
-EOF
-}
-
-delmon(){
-/usr/bin/python3 << EOF
-import base64,platform,json,re,uuid,time,subprocess,socket,sys,os
-from datetime import datetime
-from modules import makerequest,classes,configs
-
-if platform.system()=="Linux":
-   from modules import lin_utils,lin_packages
-elif platform.system()=="Windows":
-   from modules import win_utils
-else:
-   exit()
-
-QMGR="$1"
-QUEUE="$2"
-
-try:
-   mon_data = configs.getmonData()
-except Exception as err:
-   print("No such configuration file - config/confmon.json")
-mon_data[QMGR].pop(QUEUE, None)
-with open(os.getcwd()+"/config/confmon.json", 'w+') as mon_file:
-   json.dump(mon_data, mon_file)
-print(QUEUE+" configuration deleted")
-EOF
-}
-
 case "$1" in
-	 addmonq )
-      addmon $2 $3 $4 $5
-      ;;
-    addmonchl )
-      addmon $2 $3 "CHANNELS"
-      ;;
-    delmonq )
-      if [ -z "$2" ]
-      then
-        $0
-        exit 1
-      fi
-      delmon $2 $3
-      ;;
-    delmonchl )
-      if [ -z "$2" ]
-      then
-        $0
-        exit 1
-      fi
-      delmon $2 $3
-      ;;
-    enabletrackqm )
-      if [ -z "$2" ]
-      then
-        echo "Empty Qmanager"
-        exit 1
-      fi
-      sudo su - mqm -c "echo 'ALTER QMGR ACTVTRC(ON)' | $RUNMQSC $2"
-      enabletrackqm $2
-      ;;
-    disabletrackqm )
-      if [ -z "$2" ]
-      then
-        echo "Empty Qmanager"
-        exit 1
-      fi
-      sudo su - mqm -c "echo 'ALTER QMGR ACTVTRC(OFF)' | $RUNMQSC $2"
-      disabletrackqm $2
-      ;;
     help )
-      echo ""
-      echo "Usage:"
-      echo " - $0 addconf QMGR WebUser(Optional) WebPassword(Optional)"
-      echo " - $0 addmonq QMGR QUEUE TYPE_OF_MON THRESHOLD"
-      echo " - $0 delmonq QMGR QUEUE"
-      echo " - $0 enabletrackqm QMGR # Transfer the mqat.ini file to /var/mqm/qmgr/QMGR/ folder"
-      echo " - $0 disabletrackqm QMGR"
-      echo " - $0 addmonchl QMGR CHANNEL"
-      echo " - $0 delmonchl QMGR CHANNEL"
-      echo ""
-      echo "Parameters:"
-      echo " - TYPE_OF_MON: QAGE - monitor queue age, QFULL - monitor percentage of maxdepth/curdepth"
-      echo " -   THRESHOLD: threshold in percent for QFULL/seconds for QAGE (more than this value will be taken in the report)"
+      echo "Cronjobs for MWAdmin"
+      echo "Used for background processes"
       ;;
     * )
       load_config
-#      runmqjob
+      if [ -n "$mainver" ]; then
+         runmqjob
+         runmqtracker
+      fi
       if [ $HOUR == "2359" ]; then
         resetappavl
       else
         runappavl
       fi
-      runmqtracker
       ;;
    esac
--- automation/ansible/roles/midleoagent/templates/python/magent.sh
+++ automation/ansible/roles/midleoagent/templates/python/magent.sh
@@ -3,8 +3,72 @@
 #script for midleo.CORE agent
 #created by V.Vasilev
 #https://vasilev.link
+
 cd $(dirname $0)
 USR=`whoami`
+
+HOMEDIR=$(pwd)"/config"
+DSPMQVER=/opt/mqm/bin/dspmqver
+DSPMQ=/opt/mqm/bin/dspmq
+RUNMQSC=/opt/mqm/bin/runmqsc
+AMQSEVT=/opt/mqm/bin/amqsevt
+ACEUSR=mqbrk
+MQSIPROFILE=/opt/ibm/ace/server/bin/mqsiprofile
+
+export DSPMQ
+export DSPMQVER
+export AMQSEVT
+export RUNMQSC
+export ACEUSR
+export MQSIPROFILE
+
+YM=$(date '+%Y-%m')
+WD=$(date '+%d')
+HOUR=$(date '+%H%M')
+
+load_config ()
+{
+   if [ -e "$HOMEDIR/midleoclient.conf" ]; then
+      . $HOMEDIR/midleoclient.conf
+   else
+      create_config
+   fi
+}
+create_config (){
+    echo "Midleo DNS server name:"
+    read midleodns
+    if [ -z "${midleodns}" ]; then
+       rm $HOMEDIR/"midleoclient.conf"
+    else
+       echo "midleodns=$midleodns" > $HOMEDIR/"midleoclient.conf"
+    fi
+    echo "IBM MQ library path(example - /opt/mqm/java/lib64):"
+    read ibmmqlibpath
+    if [ -z "${ibmmqlibpath}" ]; then
+       rm $HOMEDIR/"midleoclient.conf"
+    else
+       echo ibmmqlibpath=$ibmmqlibpath >> $HOMEDIR/"midleoclient.conf"
+    fi
+    if test -f "$DSPMQVER"; then
+      mainver=`$DSPMQVER | grep Version | awk '{print $2}' | cut -d '.' -f 1`
+      echo mainver=$mainver >> $HOMEDIR/"midleoclient.conf"
+    fi
+    if [ -n "$mainver" ] && [ $mainver -gt 8 ]; then
+      echo mqwebhost=`hostname -f` >> $HOMEDIR/"midleoclient.conf"
+      echo "IBM MQWeb user:"
+      read ibmmqwebusr
+      echo ibmmqwebusr=$ibmmqwebusr >> $HOMEDIR/"midleoclient.conf"
+      echo "IBM MQWeb password:"
+      read -s ibmmqwebusrpwd
+      echo ibmmqwebusrpwd=`echo $ibmmqwebusrpwd | base64`  >> $HOMEDIR/"midleoclient.conf"
+      echo "IBM MQWeb SSL enabled(y/n):"
+      read ibmmqwebssl
+      echo ibmmqwebssl=$ibmmqwebssl >> $HOMEDIR/"midleoclient.conf"
+      echo "IBM MQWeb port:"
+      read mqwebport
+      echo mqwebport=$mqwebport >> $HOMEDIR/"midleoclient.conf"
+    fi
+}
 
 addcert(){
 /usr/bin/python3 << EOF
@@ -277,6 +341,139 @@ except Exception as err:
 EOF
 }
 
+enabletrackqm (){
+/usr/bin/python3 << EOF
+import base64,platform,json,re,uuid,time,subprocess,socket,sys,os
+from datetime import datetime
+from modules import makerequest,classes,configs
+
+if platform.system()=="Linux":
+   from modules import lin_utils,lin_packages
+elif platform.system()=="Windows":
+   from modules import win_utils
+else:
+   exit()
+
+QMGR="$1"
+
+def createTrackJson():
+   try:
+      track_data = configs.gettrackData()
+   except Exception as err:
+      track_data = {}
+
+   track_data[QMGR] = {
+    "type": "enabled"
+   }
+
+   with open(os.getcwd()+"/config/conftrack.json", 'w+') as track_file:
+      json.dump(track_data, track_file)
+   print(QMGR+" have been added")
+    
+if __name__ == "__main__":
+   createTrackJson()
+
+EOF
+}
+
+disabletrackqm (){
+/usr/bin/python3 << EOF
+import base64,platform,json,re,uuid,time,subprocess,socket,sys,os
+from datetime import datetime
+from modules import makerequest,classes,configs
+
+if platform.system()=="Linux":
+   from modules import lin_utils,lin_packages
+elif platform.system()=="Windows":
+   from modules import win_utils
+else:
+   exit()
+
+QMGR="$1"
+
+try:
+   track_data = configs.gettrackData()
+except Exception as err:
+   print("No such configuration file - config/conftrack.json")
+track_data.pop(QMGR, None)
+with open(os.getcwd()+"/config/conftrack.json", 'w+') as track_file:
+   json.dump(track_data, track_file)
+print(QMGR+" configuration deleted")
+EOF
+}
+
+addmon(){
+/usr/bin/python3 << EOF
+import base64,platform,json,re,uuid,time,subprocess,socket,sys,os
+from datetime import datetime
+from modules import makerequest,classes,configs
+
+if platform.system()=="Linux":
+   from modules import lin_utils,lin_packages
+elif platform.system()=="Windows":
+   from modules import win_utils
+else:
+   exit()
+
+QMGR="$1"
+QUEUE="$2"
+TMON="$3"
+THRES="$4"
+
+def createMonJson():
+   try:
+      mon_data = configs.getmonData()
+   except Exception as err:
+      mon_data = {}
+   try:
+     mon_data[QMGR][QUEUE] = {
+      "type": TMON,
+      "thres": THRES
+     }
+   except Exception as err: 
+     mon_data[QMGR] = {}
+     mon_data[QMGR][QUEUE] = {
+      "type": TMON,
+      "thres": THRES
+     }
+
+   with open(os.getcwd()+"/config/confmon.json", 'w+') as mon_file:
+      json.dump(mon_data, mon_file)
+   print(QUEUE+" on "+QMGR+" have been added")
+    
+if __name__ == "__main__":
+   createMonJson()
+
+EOF
+}
+
+delmon(){
+/usr/bin/python3 << EOF
+import base64,platform,json,re,uuid,time,subprocess,socket,sys,os
+from datetime import datetime
+from modules import makerequest,classes,configs
+
+if platform.system()=="Linux":
+   from modules import lin_utils,lin_packages
+elif platform.system()=="Windows":
+   from modules import win_utils
+else:
+   exit()
+
+QMGR="$1"
+QUEUE="$2"
+
+try:
+   mon_data = configs.getmonData()
+except Exception as err:
+   print("No such configuration file - config/confmon.json")
+mon_data[QMGR].pop(QUEUE, None)
+with open(os.getcwd()+"/config/confmon.json", 'w+') as mon_file:
+   json.dump(mon_data, mon_file)
+print(QUEUE+" configuration deleted")
+EOF
+}
+
 case "$1" in
 	addcert )
       read -p "Tool (keytool or runmqakm):" -e TOOL
@@ -341,6 +538,46 @@ case "$1" in
       fi
       startavl $2
       ;;
+   addmonq )
+      addmon $2 $3 $4 $5
+      ;;
+   addmonchl )
+      addmon $2 $3 "CHANNELS"
+      ;;
+   delmonq )
+      if [ -z "$2" ]
+      then
+        $0
+        exit 1
+      fi
+      delmon $2 $3
+      ;;
+   delmonchl )
+      if [ -z "$2" ]
+      then
+        $0
+        exit 1
+      fi
+      delmon $2 $3
+      ;;
+   enabletrackqm )
+      if [ -z "$2" ]
+      then
+        echo "Empty Qmanager"
+        exit 1
+      fi
+      sudo su - mqm -c "echo 'ALTER QMGR ACTVTRC(ON)' | $RUNMQSC $2"
+      enabletrackqm $2
+      ;;
+   disabletrackqm )
+      if [ -z "$2" ]
+      then
+        echo "Empty Qmanager"
+        exit 1
+      fi
+      sudo su - mqm -c "echo 'ALTER QMGR ACTVTRC(OFF)' | $RUNMQSC $2"
+      disabletrackqm $2
+      ;;
    * )
       echo ""
       echo "usage:"
@@ -352,4 +589,16 @@ case "$1" in
       echo "   -  $0 disableavl APP_SERVER"
       echo "   -  $0 stopavl APP_SERVER comment"
       echo "   -  $0 startavl APP_SERVER"
+      echo "   -  $0 addconf QMGR WebUser(Optional) WebPassword(Optional)"
+      echo "   -  $0 addmonq QMGR QUEUE TYPE_OF_MON THRESHOLD"
+      echo "   -  $0 delmonq QMGR QUEUE"
+      echo "   -  $0 enabletrackqm QMGR # Transfer the mqat.ini file to /var/mqm/qmgr/QMGR/ folder"
+      echo "   -  $0 disabletrackqm QMGR"
+      echo "   -  $0 addmonchl QMGR CHANNEL"
+      echo "   -  $0 delmonchl QMGR CHANNEL"
+      echo ""
+      echo "Parameters:"
+      echo "   -  TYPE_OF_MON: QAGE - monitor queue age, QFULL - monitor percentage of maxdepth/curdepth"
+      echo "   -  THRESHOLD: threshold in percent for QFULL/seconds for QAGE (more than this value will be taken in the report)"
+      
    esac
