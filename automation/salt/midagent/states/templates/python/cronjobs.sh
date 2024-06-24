634c83227d7aad0d5157d788f2a8594f5c152ed3
Vasilev <vasil@vasilev.link>
2024-06-24 16:50:12 +0200

    moved to cronjob

--- automation/salt/midagent/states/templates/python/cronjobs.sh
+++ automation/salt/midagent/states/templates/python/cronjobs.sh
@@ -38,6 +38,16 @@ if [[ ! -e $HOMEDIR ]]; then
     mkdir $HOMEDIR
 fi
 
+LRFILE=$HOMEDIR"/nextrun.txt"
+if [ -e "$LRFILE" ]; then
+  TST=$(head -n 1 $LRFILE)
+  if [ -z "$TST" ]; then
+     TST=$(date '+%Y-%m-%dT%H:%M')
+  fi
+else
+  TST=$(date '+%Y-%m-%dT%H:%M')
+fi
+
 runappavl(){
 if [ -e "$HOMEDIR/confavl.json" ]; then
   $PYTHON << EOF
@@ -251,6 +261,82 @@ except Exception as err:
 EOF
 }
 
+getsrvdata(){
+$PYTHON << EOF
+import base64,platform,json,re,time,subprocess,socket,os,zlib, glob
+from datetime import datetime, timedelta, timezone
+from modules.base import makerequest,decrypt,classes,certcheck,configs,file_utils,statarr
+from midleo_client import AGENT_VER
+
+if platform.system()=="Linux":
+   from modules.base import lin_utils,lin_packages
+elif platform.system()=="Windows":
+   from modules.base import win_utils
+else:
+   exit()
+
+
+def create():
+    try:
+        config_data = configs.getcfgData()
+        uid = config_data['uid']
+        groupid = config_data['groupid']
+        updint = config_data['updint']
+        inttoken = config_data['inttoken']
+        
+        if platform.system()=="Windows":
+            cpu = classes.CPU(win_utils.getCPUName(), win_utils.getCPUCoreCount())
+            hw_config = classes.HWConfig(win_utils.getName(), win_utils.getOS(), win_utils.getArchitecture(), win_utils.getMachineType(), cpu.__dict__, win_utils.getMemory(), win_utils.getDiskPartitions(), win_utils.getLBTS())
+            net_config = classes.NetConfig(win_utils.getIP())
+            config = classes.Config(uid,inttoken,groupid,AGENT_VER,updint, hw_config.__dict__, net_config.__dict__, win_utils.getSoftware()) 
+        elif platform.system()=="Linux":
+            cpu = classes.CPU(lin_utils.getCPUName(), lin_utils.getCPUCoreCount())
+            hw_config = classes.HWConfig(lin_utils.getName(), lin_utils.getOS(), lin_utils.getArchitecture(), lin_utils.getMachineType(), cpu.__dict__, lin_utils.getMemory(), lin_utils.getDiskPartitions(), lin_utils.getLBTS())
+            net_config = classes.NetConfig(lin_utils.getIP())
+            if os.path.isfile(os.getcwd()+"/config/certs.json"):
+               cert_check = certcheck.Run(uid+uid+uid+uid)
+            else:
+               cert_check = []
+            config = classes.Config(uid,inttoken,groupid,AGENT_VER,updint, hw_config.__dict__, net_config.__dict__, lin_packages.getSoftware(), cert_check) 
+        else:
+            exit()
+
+        return config
+    except OSError as err:
+        classes.Err("Error in create:"+str(err))
+    except Exception as ex:
+        classes.Err("Exception in create:"+str(ex))
+
+def main():
+    config = create()
+    config_data = configs.getcfgData()
+    website = config_data['website']
+    webssl = config_data['webssl']
+    updint = int(config_data['updint'])
+
+    if 'error' in config.__dict__.keys():
+        return
+
+    try:
+        output = json.dumps(config.__dict__)
+        output = re.sub(r"<([a-zA-Z-_]+)?.([a-zA-Z-_]+)(\d?):(\s?)", "", output)
+        output = re.sub(r">", "", output)
+        makerequest.postData(webssl,website,json.loads(output))
+        timenow=datetime.now() + timedelta(minutes=updint)
+
+        with open(os.getcwd()+"/config/nextrun.txt", 'w+') as log_file:
+            log_file.write(str(timenow.strftime('%Y-%m-%dT%H:%M')))
+
+    except OSError as err:
+        classes.Err("Error in main:"+str(err))
+    except Exception as ex:
+        classes.Err("Exception in main:"+str(ex))
+
+if __name__ == '__main__':
+   main()
+
+EOF
+}
 
 case "$1" in
     help )
@@ -272,6 +358,9 @@ case "$1" in
         resetappavl
       else
         runappavl
+      fi
+      if [ $TST == $(date '+%Y-%m-%dT%H:%M') ]; then
+         getsrvdata
       fi
       ;;
    esac
--- automation/salt/midagent/states/templates/python/midleo_client.py
+++ automation/salt/midagent/states/templates/python/midleo_client.py
@@ -1,79 +1,12 @@
-import base64,platform,json,re,time,subprocess,socket,os,zlib, glob
-from multiprocessing import Process
+import base64,json,subprocess,socket,os,zlib
 from datetime import datetime
-from modules.base import makerequest,decrypt,classes,certcheck,configs,file_utils,statarr
+from modules.base import decrypt,classes,configs
 
 PORT_NUMBER = 5550
 SIZE = 1024
 AGENT_VER = "1.24.10"
 
-if platform.system()=="Linux":
-   from modules.base import lin_utils,lin_packages
-elif platform.system()=="Windows":
-   from modules.base import win_utils
-else:
-   exit()
-
-
-def create():
-    try:
-        config_data = configs.getcfgData()
-        uid = config_data['uid']
-        groupid = config_data['groupid']
-        updint = config_data['updint']
-        inttoken = config_data['inttoken']
-        
-
-        if platform.system()=="Windows":
-            cpu = classes.CPU(win_utils.getCPUName(), win_utils.getCPUCoreCount())
-            hw_config = classes.HWConfig(win_utils.getName(), win_utils.getOS(), win_utils.getArchitecture(), win_utils.getMachineType(), cpu.__dict__, win_utils.getMemory(), win_utils.getDiskPartitions(), win_utils.getLBTS())
-            net_config = classes.NetConfig(win_utils.getIP())
-            config = classes.Config(uid,inttoken,groupid,AGENT_VER,updint, hw_config.__dict__, net_config.__dict__, win_utils.getSoftware()) 
-        elif platform.system()=="Linux":
-            cpu = classes.CPU(lin_utils.getCPUName(), lin_utils.getCPUCoreCount())
-            hw_config = classes.HWConfig(lin_utils.getName(), lin_utils.getOS(), lin_utils.getArchitecture(), lin_utils.getMachineType(), cpu.__dict__, lin_utils.getMemory(), lin_utils.getDiskPartitions(), lin_utils.getLBTS())
-            net_config = classes.NetConfig(lin_utils.getIP())
-            if os.path.isfile(os.getcwd()+"/config/certs.json"):
-               cert_check = certcheck.Run(uid+uid+uid+uid)
-            else:
-               cert_check = []
-            config = classes.Config(uid,inttoken,groupid,AGENT_VER,updint, hw_config.__dict__, net_config.__dict__, lin_packages.getSoftware(), cert_check) 
-        else:
-            exit()
-
-        return config
-    except OSError as err:
-        classes.Err("Error in create:"+str(err))
-    except Exception as ex:
-        classes.Err("Exception in create:"+str(ex))
-
-def main():
-    config = create()
-    config_data = configs.getcfgData()
-    website = config_data['website']
-    webssl = config_data['webssl']
-    updint = config_data['updint']
-
-    if 'error' in config.__dict__.keys():
-        return
-
-    try:
-        output = json.dumps(config.__dict__)
-        output = re.sub(r"<([a-zA-Z-_]+)?.([a-zA-Z-_]+)(\d?):(\s?)", "", output)
-        output = re.sub(r">", "", output)
-        makerequest.postData(webssl,website,json.loads(output))
-        return updint
-    except OSError as err:
-        classes.Err("Error in main:"+str(err))
-    except Exception as ex:
-        classes.Err("Exception in main:"+str(ex))
-
 configs.createConfigJson()
-
-def funcgetdata():
-    while True:
-        getupdint=main()
-        time.sleep(int(getupdint)*60)
 
 def listenfordata():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -143,8 +76,4 @@ def listenfordata():
 if __name__ == '__main__':
 
     classes.ClearLog()
-    proc1 = Process(target=funcgetdata)
-    proc1.start()
-
-    proc2 = Process(target=listenfordata)
-    proc2.start()
+    listenfordata()
