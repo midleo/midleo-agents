9ed364bf707591ce6f00bd3fd25272a9cff0f098
Midleo Team <team@midleo.com>
2025-05-14 12:00:12 +0200

    fixed statistics

--- automation/salt/midagent/states/templates/python/modules/base/statarr.py
+++ automation/salt/midagent/states/templates/python/modules/base/statarr.py
@@ -76,8 +76,30 @@ def avlCheck(thisapp,dcont="",cred=None):
     arr["ibmiibdocker"]="/usr/bin/docker exec -t "+dcont+" /bin/bash -c '. "+os.environ['IIBMQSIPROFILE']+" &&  mqsilist' | grep "+thisapp+" | grep running | wc -l"
     arr["axwaycft"]="ps -ef|grep CFTMAIN | wc -l"
     arr["axwayst"]="ps -ef|grep Axway | grep catalina | wc -l"
-    arr["kafka"]="java -jar "+os.environ['MWAGTDIR']+"/modules/statistics/kafka/resources/midleo_kafka.jar '{\"function\":\"srvcheck\",\"server\":\""+thisapp+"\"}' | grep "+thisapp+" | wc -l"
-    arr["jboss"]="java -jar "+os.environ['MWAGTDIR']+"/modules/statistics/jboss/resources/midleo_jboss.jar '{\"function\":\"srvcheck\",\"server\":\""+thisapp+"\",\"usr\":\""+cred.get("usr", default_usr)+"\",\"pwd\":\""+cred.get("pwd", default_pwd)+"\",\"mngmport\":\""+cred.get("mngmport", default_mngmport)+"\"}' | grep "+thisapp+" | wc -l"
-    arr["tomcat"]="java -jar "+os.environ['MWAGTDIR']+"/modules/statistics/tomcat/resources/midleo_tomcat.jar '{\"function\":\"srvcheck\",\"server\":\""+thisapp+"\",\"usr\":\""+cred.get("usr", default_usr)+"\",\"pwd\":\""+cred.get("pwd", default_pwd)+"\",\"mngmport\":\""+cred.get("mngmport", default_mngmport)+"\"}' | grep "+thisapp+" | wc -l"
-    arr["activemq"]="java -jar "+os.environ['MWAGTDIR']+"/modules/statistics/activemq/resources/midleo_activemq.jar '{\"function\":\"srvinfo\",\"server\":\""+thisapp+"\",\"usr\":\""+cred.get("usr", default_usr)+"\",\"pwd\":\""+cred.get("pwd", default_pwd)+"\",\"mngmport\":\""+cred.get("jmxport", default_mngmport)+"\"}' | grep "+thisapp+" | wc -l"
+    arr["kafka"] = (
+      "java -cp '/midleolibs/libs/*:" + os.environ['MWAGTDIR'] +
+      "/modules/statistics/kafka/resources/midleo_kafka.jar' midleo_kafka.kafka_main " +
+      "'{\"function\":\"srvcheck\",\"server\":\"" + thisapp + "\"}' | grep " + thisapp + " | wc -l"
+    )
+    arr["jboss"] = (
+      "java -cp '/midleolibs/libs/*:" + os.environ['MWAGTDIR'] +
+      "/modules/statistics/jboss/resources/midleo_jboss.jar' midleo_jboss.midleo_jboss " +
+      "'{\"function\":\"srvcheck\",\"server\":\"" + thisapp + "\",\"usr\":\"" + cred.get("usr", default_usr) +
+      "\",\"pwd\":\"" + cred.get("pwd", default_pwd) + "\",\"mngmport\":\"" + cred.get("mngmport", default_mngmport) +
+      "\"}' | grep " + thisapp + " | wc -l"
+    )
+    arr["tomcat"] = (
+      "java -cp '/midleolibs/libs/*:" + os.environ['MWAGTDIR'] +
+      "/modules/statistics/tomcat/resources/midleo_tomcat.jar' midleo_tomcat.midleo_tomcat " +
+      "'{\"function\":\"srvcheck\",\"server\":\"" + thisapp + "\",\"usr\":\"" + cred.get("usr", default_usr) +
+      "\",\"pwd\":\"" + cred.get("pwd", default_pwd) + "\",\"mngmport\":\"" + cred.get("mngmport", default_mngmport) +
+      "\"}' | grep " + thisapp + " | wc -l"
+    )
+    arr["activemq"] = (
+      "java -cp '/midleolibs/libs/*:" + os.environ['MWAGTDIR'] +
+      "/modules/statistics/activemq/resources/midleo_activemq.jar' midleo_activemq.activemq_main " +
+      "'{\"function\":\"srvinfo\",\"server\":\"" + thisapp + "\",\"usr\":\"" + cred.get("usr", default_usr) +
+      "\",\"pwd\":\"" + cred.get("pwd", default_pwd) + "\",\"mngmport\":\"" + cred.get("jmxport", default_mngmport) +
+      "\"}' | grep " + thisapp + " | wc -l"
+    )
     return arr
