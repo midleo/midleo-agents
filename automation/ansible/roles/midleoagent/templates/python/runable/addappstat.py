19c37a36790b7c631eff5fe012f45cd0f21d0868
Midleo Team <team@midleo.com>
2026-03-16 15:50:23 +0100

    encrypt password not base64

--- automation/ansible/roles/midleoagent/templates/python/runable/addappstat.py
+++ automation/ansible/roles/midleoagent/templates/python/runable/addappstat.py
@@ -1,41 +1,39 @@
 import base64
-import json,sys,os,inspect
+import json, sys, os, inspect
 
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
-sys.path.insert(0, parentdir) 
+sys.path.insert(0, parentdir)
 
-from modules.base import configs
+from modules.base import decrypt, configs
 
-SRVTYPE=sys.argv[1]
-APPSRV=sys.argv[2]
-STDATA=sys.argv[3]
+SRVTYPE = sys.argv[1]
+APPSRV = sys.argv[2]
+STDATA = sys.argv[3]
+
 
 def createMonJson():
-   try:
-      mon_data = configs.getmonData()
-   except Exception as err:
-      mon_data = {}
-   try:
-      STJSDATA=json.loads(STDATA)
-   except Exception as err:
-      STJSDATA={}
-   try:
-     for k,item in STJSDATA.items():
-         mon_data[SRVTYPE][APPSRV][k] = base64.b64encode(item.encode()).decode('ascii') if k=='pwd' else item
+    try:
+        mon_data = configs.getmonData()
+    except Exception:
+        mon_data = {}
 
-   except Exception as err: 
-     try:
-        mon_data[SRVTYPE][APPSRV] = {}
-     except Exception as err:
+    try:
+        STJSDATA = json.loads(STDATA)
+    except Exception:
+        STJSDATA = {}
+
+    if SRVTYPE not in mon_data:
         mon_data[SRVTYPE] = {}
+    if APPSRV not in mon_data[SRVTYPE]:
         mon_data[SRVTYPE][APPSRV] = {}
-     for k,item in STJSDATA.items():
-         mon_data[SRVTYPE][APPSRV][k] = base64.b64encode(item.encode()).decode('ascii') if k=='pwd' else item
+    for k, item in STJSDATA.items():
+        mon_data[SRVTYPE][APPSRV][k] = (
+            decrypt.encryptPWD(item) if k == "pwd" and item else item
+        )
+    with open(os.getcwd() + "/config/confapplstat.json", "w+") as mon_file:
+        json.dump(mon_data, mon_file)
+    print(APPSRV + " of type " + SRVTYPE + " have been added")
 
-   with open(os.getcwd()+"/config/confapplstat.json", 'w+') as mon_file:
-      json.dump(mon_data, mon_file)
-   print(APPSRV+" of type "+SRVTYPE+" have been added")
-    
 if __name__ == "__main__":
-   createMonJson()
+    createMonJson()
--- monitoring.beats/fluentbit/enrich.lua
+++ monitoring.beats/fluentbit/enrich.lua
@@ -1,6 +1,35 @@
 cache = {}
 ttl = 60
 
+local function extract_message_after_error(line, error)
+    if not line or line == "" then
+        return ""
+    end
+
+    line = string.gsub(line, "^#+%s*", "")
+    line = string.gsub(line, "[\r\n]+", " ")
+
+    if error and error ~= "" then
+        local _, finish = string.find(line, "<" .. error .. ">", 1, true)
+        if finish then
+            local rest = string.sub(line, finish + 1)
+            local msg = string.match(rest, "%s*<([^<>].-)>%s*$")
+            if msg and msg ~= "" then
+                return msg
+            end
+        end
+    end
+
+    local last = nil
+    for part in string.gmatch(line, "<([^<>]*)>") do
+        if part and part ~= "" then
+            last = part
+        end
+    end
+
+    return last or line
+end
+
 function enrich_record(tag, timestamp, record)
 
     local error = record["error"]
@@ -10,9 +39,11 @@ function enrich_record(tag, timestamp, record)
         return -1, timestamp, record
     end
 
+    local clean_message = extract_message_after_error(line, error)
+
     record["monid"] = "weblogic_" .. error
     record["alerttime"] = os.date("%Y-%m-%d %H:%M:%S")
-    record["message"] = string.sub(line,1,500)
+    record["message"] = string.sub(clean_message, 1, 500)
 
     record["rawlog"] = nil
     record["log"] = nil
